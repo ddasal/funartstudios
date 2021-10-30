ea2a79413ebd86ca081c24d66d23b675d98fec51
Dustin Dasal <ddasal@gmail.com>
2021-10-30 16:47:28 -0500

    Environment setup

--- manage.py
+++ manage.py
@@ -2,10 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv
+import pathlib
 
 def main():
     """Run administrative tasks."""
+        #### added for .env support getting ready for production
+    DOT_ENV_PATH = pathlib.Path() / '.env'
+    if DOT_ENV_PATH.exists():
+        dotenv.read_dotenv(str(DOT_ENV_PATH))
+    else:
+        print('No .env found, be sure to add it.')
+    ####
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funartstudios.settings')
     try:
         from django.core.management import execute_from_command_line
