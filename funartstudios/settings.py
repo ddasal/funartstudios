8fdf12f389324687d99b4a73e71bfecd5830f3f7
Dustin Dasal <ddasal@gmail.com>
2021-11-15 21:24:07 -0600

    Heroku Readiness

--- funartstudios/settings.py
+++ funartstudios/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -206,3 +207,5 @@ from .cdn.conf import * # noqa
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
