319e92e6c77fadc7395814b791f78864776fc0a1
Dustin Dasal <ddasal@gmail.com>
2021-11-24 13:04:19 -0600

    dotenv

--- funartstudios/settings.py
+++ funartstudios/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import django_heroku
+# import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -208,4 +208,4 @@ from .cdn.conf import * # noqa
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
--- funartstudios/wsgi.py
+++ funartstudios/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import dotenv
+import pathlib
 from django.core.wsgi import get_wsgi_application
+
+DOT_ENV_PATH = pathlib.Path() / '.env'
+if DOT_ENV_PATH.exists():
+    dotenv.read_dotenv(str(DOT_ENV_PATH))
+else:
+    print('No .env found, be sure to add it.')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funartstudios.settings')
 
