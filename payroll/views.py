c0c7afbd8735b66a8670fb3ec3c314b8bfa9231d
Dustin Dasal <ddasal@gmail.com>
2021-11-28 21:37:24 -0600

    debugging

--- payroll/views.py
+++ payroll/views.py
@@ -251,7 +251,7 @@ def report_detail_hx_view(request, id=None):
     except Exception as e:
         import sys
         obj = None
-        msg = e + "on line {}".format(sys.exc_info()[-1].tb_lineno)
+        msg = "error on line {}".format(sys.exc_info()[-1].tb_lineno)
     if obj is None:
         return HttpResponse(msg)
     context = {
