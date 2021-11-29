6b9a5d6719a307f3747ed0b443c0413abafc35eb
Dustin Dasal <ddasal@gmail.com>
2021-11-29 14:30:04 -0600

    Added Distributor

--- products/models.py
+++ products/models.py
@@ -106,6 +106,7 @@ def product_post_save(sender, instance, created, *args, **kwargs):
 post_save.connect(product_post_save, sender=Product)
 
 class Suppliers(models.TextChoices):
+    ARTBOARDSDIRECT = 'b', 'Art Boards Direct'
     ARTDISTRIBUTORS = 'a', 'Art Supply Distributors'
     DIVERSEWOOD = 'd', 'Diverse Woodworking'
     HOBBYLOBBY = 'h', 'Hobby Lobby'
