9538ad98b698d5fb5c5e9333f1bcbe02a732590c
Dustin Dasal <ddasal@gmail.com>
2022-04-24 18:16:28 -0500

    logs

--- util/views.py
+++ util/views.py
@@ -7,8 +7,14 @@ def offsite_backup(request):
     # create tarball for offloading
     obj = logs.objects.create()
     with tarfile.open('archive.tar.gz', 'w:gz') as archive:
-        archive.add('/home/ubuntu/backups/')
-        archive.add('/home/ubuntu/funartstudios/')
+        try:
+            archive.add('/home/ubuntu/backups/')
+        except:
+            pass
+        try:
+            archive.add('../funartstudios/')
+        except:
+            pass
         print('file created')
         logs.objects.filter(pk=obj.pk).update(user=request.user, log=archive.list)
         print('log written')
